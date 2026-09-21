f3755027402eb66fe45566e71188a0181726ff96
Smriti Singh <smritisingh1818@gmail.com>
2026-09-21 11:49:20 +0530

    Complete JWT authentication

--- src/main/java/com/payment/wallet_system/config/SecurityConfig.java
+++ src/main/java/com/payment/wallet_system/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.payment.wallet_system.config;
 
+import com.payment.wallet_system.security.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.payment.wallet_system.security.JwtAuthenticationFilter;
 
 @Configuration 
 public class SecurityConfig {
 
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  public  SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+   this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+  }
     @Bean 
       public  PasswordEncoder passwordEncoder(){ // interace
       return new  BCryptPasswordEncoder();// actual implementation
@@ -23,7 +31,8 @@ public class SecurityConfig {
             .requestMatchers("/api/users","/api/auth/login").permitAll()
             .anyRequest().authenticated()
 
-          );
+          )
+          .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return  http.build();
     }
--- src/main/java/com/payment/wallet_system/security/JwtService.java
+++ src/main/java/com/payment/wallet_system/security/JwtService.java
@@ -31,5 +31,22 @@ public class JwtService {
         .expiration(new  Date(System.currentTimeMillis()+1000*60*60))
         .signWith(getSigningKey())
         .compact();
+
+
     }
+     public String extractEmail(String token){
+        return Jwts.parser()
+                 .verifyWith(getSigningKey())
+                 .build()
+                 .parseSignedClaims(token)
+                 .getPayload()
+                 .getSubject();
+
+     }
+     public  boolean isTokenValid(String token ,User user){
+        String email=extractEmail(token);
+        return email.equals(user.getEmail());
+     }
+
+   
 }
